aa2e1056ce369693ae480f99ce9e6d342c75c703
Paulo Oliver <134943577+rootaccess2023@users.noreply.github.com>
2024-08-08 12:51:28 +0200

    layout: added the layout for map

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,30 @@
 class PlacesController < ApplicationController
   def index
-    @places = Place.all
+    year = params[:year]
+    @places = if year.nil?
+                  Place.where("is_2017", true)
+                 else
+                  Place.where("is_#{year} = ?", true)
+                 end
+  end
+
+  def show
+    @place = Place.find(params[:id])
+    @places = Place.where(latitude: @place.latitude, longitude: @place.longitude, name: @place.name)
+
+  end
+
+  def home
+    api_key = 'cc38c00429dc4dee93ba2451d77f9ae5'
+    client = NewsService::Client.new(api_key)
+    result = client.fetch_pogo_news
+    @news = result[:data]['articles'] if result[:status] == 'Success'
+    @error_message = result[:data] unless result[:status] == 'Success'
+  end
+
+  def report
+  end
+
+  def timeline
   end
 end
--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -2,13 +2,16 @@ import { Controller } from "@hotwired/stimulus"
 import L from "leaflet"
 
 export default class extends Controller {
-  static targets = ["container"]
+  static targets = ["container", "locationName", "locationAddress", "locationCoordinates"]
   static values = { latlong: Array }
 
+  phil_coords = [12.8797, 121.7740]
+
   connect() {
-    this.createMap()
-    this.map.fitBounds(this.latlongValue)
-    this.latlongValue.forEach(place => this.addMarker(place))
+    this.createMap();
+    this.map.setView(this.phil_coords, 6);
+    this.latlongValue.forEach(place => this.addMarker(place));
+    console.log('Map Controller connected successfully');
   }
 
   createMap() {
@@ -21,15 +24,26 @@ export default class extends Controller {
   }
 
   addMarker(place) {
-    const [latitude, longitude] = place;
-    L.marker([latitude, longitude])
+    const [latitude, longitude, name, address] = place;
+    const marker = L.marker([latitude, longitude])
       .addTo(this.map)
-      .bindPopup(`<div>latitude: ${latitude}</div><div>longitude: ${longitude}</div>`)
+      .bindPopup(`<div class="font-medium">${name}</div>`)
+
+    marker.on('click', () => {
+      this.updateLocationDetails(name, address, latitude, longitude);
+    });
   }
 
+  updateLocationDetails(name, address, latitude, longitude) {
+    this.locationNameTarget.textContent = name;
+    this.locationAddressTarget.textContent = address;
+    this.locationCoordinatesTarget.innerHTML = `
+      <p class="pr-4">N ${latitude.toFixed(5)}</p>
+      <p>E ${longitude.toFixed(5)}</p>
+    `;
+  }
 
   disconnect() {
     this.map.remove();
   }
 }
-
