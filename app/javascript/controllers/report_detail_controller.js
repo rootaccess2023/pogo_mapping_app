1127f111eddaf33680175482f9c34bfb7f1fa75c
Paulo Oliver <134943577+rootaccess2023@users.noreply.github.com>
2024-08-09 23:43:12 +0200

    styles: fixed responsiveness using stimulus js

--- app/javascript/controllers/report_detail_controller.js
+++ app/javascript/controllers/report_detail_controller.js
@@ -2,7 +2,8 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="report-detail"
 export default class extends Controller {
-  static targets = ["toggleReportDescription"];
+  static targets = ["toggleReportDescription", "toggleArrow", "toggleNavheight"];
+
   connect() {
     console.log("Connected to report-detail")
   }
@@ -10,5 +11,21 @@ export default class extends Controller {
   toggleDescription(event) {
     event.preventDefault();
     this.toggleReportDescriptionTarget.classList.toggle("hidden");
+  }
+
+  toggleArrow(event) {
+    event.preventDefault();
+    if (this.toggleArrowTarget.classList.contains("rotate-180")) {
+      this.toggleArrowTarget.classList.remove("rotate-180");
+      this.toggleArrowTarget.classList.add("rotate-360");
+    } else {
+      this.toggleArrowTarget.classList.remove("rotate-360");
+      this.toggleArrowTarget.classList.add("rotate-180");
+    }
+  }
+
+  toggleNavheight(event) {
+    event.preventDefault();
+    this.toggleNavheightTarget.classList.toggle("custom-navheight");
   }
 }
