372ec3e521d6ade16e24257b22c0b93a475f2178
redroboto <vince.pleyto@gmail.com>
2024-08-09 22:23:56 +0800

    feat: only show reports belonging to the user

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,3 +1,4 @@
 class Report < ApplicationRecord
+  validates :address, :activities, :summary, :user_id, presence: true
   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :reports
+  has_many :reports, dependent: :destroy
 end
