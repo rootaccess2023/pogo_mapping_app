e0a2e6a366fed74b00c0dba19c1eb7cb54db0e34
Paulo Oliver <134943577+rootaccess2023@users.noreply.github.com>
2024-08-09 08:21:17 +0200

    styles: added consistent styles to form

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,3 +9,6 @@ application.register("hello", HelloController)
 
 import MapController from "./map_controller"
 application.register("map", MapController)
+
+import ReportDetailController from "./report_detail_controller"
+application.register("report-detail", ReportDetailController)
